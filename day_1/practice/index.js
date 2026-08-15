2f3a49cd800be8fd2b5a8893f06a5758adcbbe83
Shefaul Islam Shefa <sishefa19@gmail.com>
2026-08-15 13:30:17 +0600

    Add day_1 JavaScript practice files

---
+++ day_1/practice/index.js
@@ -0,0 +1,33 @@
+console.log("Hello, World!");
+
+const name = "Shefaul Islam";
+const age = 25;
+
+console.log("My name is " + name + " and I am " + age + " years old.");
+
+// Practice: Variables and Data Types
+const my_name = "Shefaul Islam";
+const income = 5000;
+const expense = 2000;
+
+// Output the values of the variables
+console.log("Name: " + my_name);
+console.log("Income: " + income);
+console.log("Expense: " + expense);
+
+// Output the data types of the variables
+console.log(typeof my_name);
+console.log(typeof income);
+console.log(typeof expense);
+
+// Practice: Type Conversion
+let num = "10.50";
+console.log(typeof num);
+
+// Convert the string to a number
+num = parseFloat(num);
+console.log(typeof num);
+
+// Practice: Number Formatting
+console.log(num.toFixed(2));
+console.log(num.toPrecision(3));

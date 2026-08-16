5a764e40d49a67a9060b8222533cc94567d0479d
Shefaul Islam Shefa <sishefa19@gmail.com>
2026-08-16 13:23:43 +0600

    Add day_2 JavaScript practice

---
+++ day_2/index.js
@@ -0,0 +1,10 @@
+const userName = "Shefaul Islam";
+const income = 2500;
+const expenses = 1500;
+
+console.log("Personal Salary Details");
+console.log("-----------------------");
+console.log("User: " + userName.toUpperCase());
+console.log("Total Income: $" + income);
+console.log("Total Expenses: $" + expenses);
+console.log("Net Savings: $" + (income - expenses));

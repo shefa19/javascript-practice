eb3f05a804c4f1a0cbe9b7dacac35c269f38ae62
Shefaul Islam Shefa <sishefa19@gmail.com>
2026-08-18 22:33:04 +0600

    Add day_6 JavaScript practice

---
+++ day_6/index.js
@@ -0,0 +1,5 @@
+const n = parseInt(prompt("Enter a number: "));
+
+for (let i = 1; i <= 10; i++) {
+  document.write(`${n} * ${i} = ${i * n}<br>`);
+}
---
+++ day_6/task_6.js
@@ -0,0 +1,11 @@
+let i = 0,
+  sum = 0;
+
+while (i <= 100) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log(i);
+    sum += i;
+  }
+  i++;
+}
+console.log("Sum of all numbers : ", sum);

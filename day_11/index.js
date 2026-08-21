1b7b909f42b6dec03f1050ff369c2a3c532af9c8
Shefaul Islam Shefa <sishefa19@gmail.com>
2026-08-21 19:34:55 +0600

    Add day_11 JavaScript practice

---
+++ day_11/index.js
@@ -0,0 +1,10 @@
+// select by id
+let heading = document.getElementById("wish");
+
+heading.innerHTML = "Good Bye! Good Bye!";
+
+// select by class name
+document.getElementsByClassName("para")[0].innerHTML = "Hi";
+
+// select by tag name
+document.getElementsByTagName("h1")[1].innerHTML = "I am a new learner";

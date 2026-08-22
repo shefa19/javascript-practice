d616a493ff493f7460702f211804728ccead0d7d
Shefaul Islam Shefa <sishefa19@gmail.com>
2026-08-22 07:45:07 +0600

    Add day_12 JavaScript practice

---
+++ day_12/index.js
@@ -0,0 +1,8 @@
+// query selector by id
+document.querySelector("#pid").innerHTML = "Hello";
+
+// query selector by class
+document.querySelector(".pid").innerHTML = "Hi";
+
+// query selector by tag
+document.querySelector("h1").innerHTML = "Today I learn query selector";
---
+++ practice.js
@@ -0,0 +1,5 @@
+function number(x, y = 30) {
+  console.log(x, y);
+}
+
+number(10);
